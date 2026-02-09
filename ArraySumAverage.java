c312964f7be180fb7d1036318ddd25efe7259fc1
Sufail <132893315+sufailansari@users.noreply.github.com>
2026-02-09 21:23:28 +0530

    Add files via upload

---
+++ ArraySumAverage.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+public class ArraySumAverage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to array Sum and Average ");
+        int [] numArray = ArrayUtility.inputArray();
+
+        long sum = sum(numArray);
+        double avg  = average(numArray);
+        System.out.println("Sum of the number is :"+sum);
+        System.out.println("Average of the number is :"+avg);
+    }
+    public static long sum( int [] numArray){
+        long sum=0;
+        int i=0;
+        while (i<numArray.length){
+            sum+=numArray[i];
+            i++;
+        }
+        return sum;
+    }
+    public static double average(int [] numArray){
+        double sum = sum(numArray);
+        return (sum/numArray.length);
+    }
+
+}
